42ce2346fdfe2602311231e2ea0e2a4aab46c9de
Michal Moudrý <moudry.michal@live.com>
2023-09-19 14:42:15 +0200

    Additional workflow bug fixes

--- database/model/data_transfer_obj.go
+++ database/model/data_transfer_obj.go
@@ -18,17 +18,19 @@ type ApplicationInfo struct {
 
 // A structure encapsulating available info of a specific workflow.
 type WorkflowInfo struct {
-	Id               uuid.UUID `db:"id"`
-	Name             string    `db:"workflow_name"`
-	Settings         WorkflowSetting
-	ConcurrencyStamp uuid.UUID `db:"concurrency_stamp"`
-	Added            time.Time `db:"date_added"`
-	Updated          time.Time `db:"date_updated"`
+	Id                    uuid.UUID `db:"id"`
+	Name                  string    `db:"workflow_name"`
+	IsFullPageRecognition bool      `db:"setting_is_full_page_recog"`
+	SkipImageEnhancement  bool      `db:"setting_skip_enhancement"`
+	ExpectDifferentImages bool      `db:"setting_expect_diff_images"`
+	ConcurrencyStamp      uuid.UUID `db:"concurrency_stamp"`
+	Added                 time.Time `db:"date_added"`
+	Updated               time.Time `db:"date_updated"`
 }
 
 // A structure representing a setting of a specific recognition workflow.
 type WorkflowSetting struct {
-	IsFullPageRecognition bool `db:"setting_is_full_page_recog"`
-	SkipImageEnhancement  bool `db:"setting_skip_enhancement"`
-	ExpectDifferentImages bool `db:"setting_expect_diff_images"`
+	IsFullPageRecognition bool
+	SkipImageEnhancement  bool
+	ExpectDifferentImages bool
 }
