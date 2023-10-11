ba5c9fe41ca6ee826330bc229e3f884f5edd0e53
Michal Moudrý <moudry.michal@live.com>
2023-10-11 13:15:19 +0200

    Updated config test

--- src/config/config_test.go
+++ src/config/config_test.go
@@ -29,6 +29,7 @@ func Test_ReadConfigFromFile(t *testing.T) {
 			want: Config{
 				Port:             8080,
 				ConnectionString: "postgres://root:root@localhost:5432/workflow-serv-db?sslmode=disable",
+				Environment:      "[dev]",
 			},
 			wantErr: false,
 		},
