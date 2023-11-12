9a67c975d3d47044fa071b1f09c3703a4700eaac
Michal Moudrý <moudry.michal@live.com>
2023-11-12 19:29:44 +0100

    Updated user delete event logging (#13)

--- src/transport/users_controller.go
+++ src/transport/users_controller.go
@@ -1,6 +1,7 @@
 package transport
 
 import (
+	"log"
 	"net/http"
 	"workflow-service/transport/model/contracts"
 	"workflow-service/transport/util"
@@ -9,12 +10,15 @@ import (
 // A method for handling requests to delete all user's data.
 func (handler *Handler) DeleteUsersData(w http.ResponseWriter, r *http.Request) {
 	var eventData contracts.CloudEvent[string]
+	log.Println("User delete started...")
 	if err := util.UnmarshallRequest(r, &eventData); err != nil {
 		util.WriteErrResponse(w, http.StatusBadRequest, err)
 		return
 	}
+	log.Printf("Obtained an user-delete event for %v\n", eventData.Data)
 	err := handler.Services.UserService.DeleteUsersData(r.Context(), eventData.Data)
 	if err != nil {
+		log.Println("User delete failed")
 		util.WriteErrResponse(w, http.StatusInternalServerError, err)
 		return
 	}
